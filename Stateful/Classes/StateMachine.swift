ceacf9f94e0564a5a9c69d08b1ab4b3eacedc31c
Alberto De Bortoli <albertodebortoli@gmail.com>
2018-12-17 21:00:36 +0000

    Formatting

--- Stateful/Classes/StateMachine.swift
+++ Stateful/Classes/StateMachine.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 final public class StateMachine<State: Hashable, Event: Hashable> {
-
+    
     public var enableLogging: Bool = false
     public var currentState: State {
         return {
@@ -17,21 +17,21 @@ final public class StateMachine<State: Hashable, Event: Hashable> {
             }
             }()
     }
-
+    
     private var internalCurrentState: State
     private var transitionsByEvent: [Event : [Transition<State, Event>]] = [:]
-
+    
     private let lockQueue: DispatchQueue
     private let workingQueue: DispatchQueue
     private let callbackQueue: DispatchQueue
-
+    
     public init(initialState: State, callbackQueue: DispatchQueue? = nil) {
         self.internalCurrentState = initialState
         self.lockQueue = DispatchQueue(label: "com.albertodebortoli.statemachine.queue.lock")
         self.workingQueue = DispatchQueue(label: "com.albertodebortoli.statemachine.queue.working")
         self.callbackQueue = callbackQueue ?? .main
     }
-
+    
     public func add(transition: Transition<State, Event>) {
         lockQueue.sync {
             let transitions = self.transitionsByEvent[transition.event]
@@ -42,48 +42,48 @@ final public class StateMachine<State: Hashable, Event: Hashable> {
             }
         }
     }
-
+    
     public func process(event: Event, callback: TransitionBlock? = nil) {
         var transitions: [Transition<State, Event>]?
         lockQueue.sync {
             transitions = self.transitionsByEvent[event]
         }
-
+        
         workingQueue.async {
             let performableTransitions = transitions?.filter { return $0.source == self.internalCurrentState } ?? []
-
+            
             if performableTransitions.count == 0 {
                 self.callbackQueue.async {
                     callback?(.failure)
                 }
                 return
             }
-
+            
             for transition in performableTransitions {
                 self.log(message: "[Stateful 🦜]: Processing event '\(event)' from '\(self.internalCurrentState)'")
                 self.callbackQueue.async {
                     transition.executePreBlock()
                 }
-
+                
                 self.log(message: "[Stateful 🦜]: Processed pre condition for event '\(event)' from '\(transition.source)' to '\(transition.destination)'")
-
+                
                 let previousState = self.internalCurrentState
                 self.internalCurrentState = transition.destination
-
+                
                 self.log(message: "[Stateful 🦜]: Processed state change from '\(previousState)' to '\(transition.destination)'")
                 self.callbackQueue.async {
                     transition.executePostBlock()
                 }
-
+                
                 self.log(message: "[Stateful 🦜]: Processed post condition for event '\(event)' from '\(transition.source)' to '\(transition.destination)'")
-
+                
                 self.callbackQueue.async {
                     callback?(.success)
                 }
             }
         }
     }
-
+    
     private func log(message: String) {
         if self.enableLogging {
             print(message)
